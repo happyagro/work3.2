1844fdfc0178f8ed8051c62a7b0fbb4c217e2651
Nikita Aniskovets <happyaggro@gmail.com>
2021-04-07 23:49:53 +0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ public class Main {
         int customerAccount = 100;
         int bonusScale = 100;
         int uploadVolume = 1700;
-        int bonus;
+        int bonus = 0;
 
         if (uploadVolume >= 1000){
             bonus = uploadVolume / bonusScale;
-        } else {
-            bonus = 0;
         }
 
         int totalAccount = customerAccount + uploadVolume + bonus;
