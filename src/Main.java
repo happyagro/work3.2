7f75e825bee3c77b3d6a176d3041fffede78c780
Nikita Aniskovets <happyaggro@gmail.com>
2021-04-07 23:44:14 +0300

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+        int customerAccount = 100;
+        int bonusScale = 100;
+        int uploadVolume = 1700;
+        int bonus;
+
+        if (uploadVolume >= 1000){
+            bonus = uploadVolume / bonusScale;
+        } else {
+            bonus = 0;
+        }
+
+        int totalAccount = customerAccount + uploadVolume + bonus;
+
+        System.out.println("Вы пополнили счёт на " + uploadVolume + " руб.");
+        System.out.println("Ваш бонус составил " + bonus + " руб.");
+        System.out.println("Ваш баланс " + totalAccount + " руб.");
+    }
 }
